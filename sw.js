73c86815f063672d033caa3a0a0855b2edd2208e
Ali Albayrak <albayrakali99@gmail.com>
2023-04-20 19:11:52 +0300

    .

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-var cacheName = 'cache-v1';
-var cacheFiles = ['/', '/assets/css/main.css', '/assets/scripts/main.js'];
+// var cacheName = 'cache-v1';
+// var cacheFiles = ['/', '/assets/css/main.css', '/assets/scripts/main.js'];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            console.log('Opened cache');
-            return cache.addAll(cacheFiles);
-        })
-    );
-});
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(
+//         caches.open(cacheName).then((cache) => {
+//             console.log('Opened cache');
+//             return cache.addAll(cacheFiles);
+//         })
+//     );
+// });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            if (response) return response;
-            return fetch(event.request);
-        }
-        )
-    );
-});
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//         caches.match(event.request).then((response) => {
+//             if (response) return response;
+//             return fetch(event.request);
+//         }
+//         )
+//     );
+// });
 
-self.addEventListener("activate", (event) => {
-    console.log("Activating Service Worker...");
-    return self.clients.claim();
-});
+// self.addEventListener("activate", (event) => {
+//     console.log("Activating Service Worker...");
+//     return self.clients.claim();
+// });
