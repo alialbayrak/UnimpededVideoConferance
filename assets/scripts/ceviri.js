cd4b00c02c525d31aa98e0ef027486fab45ede4d
Ali Albayrak <albayrakali99@gmail.com>
2023-04-20 19:09:55 +0300

    çeviri

--- assets/scripts/ceviri.js
+++ assets/scripts/ceviri.js
@@ -1,49 +1,147 @@
 import * as mainjs from '/assets/scripts/main.js'
 
 
-
+const list = [
+    {
+        text: 'nasılsın',
+        file: 'how-are-you-signtime.mp4'
+    },
+    {
+        text: 'üzgünüm',
+        file: 'sorry-sign-language.mp4'
+    },
+    {
+        text: 'lütfen',
+        file: 'please-sign-language.mp4'
+    },
+    {
+        text: 'seni seviyorum',
+        file: 'i-love-you-sign-language.mp4'
+    },
+    {
+        text: 'evet',
+        file: 'yes-sign.mp4'
+    },
+    {
+        text: 'hayır',
+        file: 'no-sign-language.mp4'
+    },
+    {
+        text: 'anladım',
+        file: 'understand-signtime.mp4'
+    }
+]
 
 $(async () => {
 
-    let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    // let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
     //START SES KAYDETME
-    const audioRecorder = new MediaRecorder(stream);
+    // const audioRecorder = new MediaRecorder(stream);
     const microphone = $("#microphone");
     const microphoneText = $("#text-microphone");
+    const video = $('#video')[0]
+    video.onplay = function () {
+        if (loopList && loopList.length > 0) {
+            let htmlText = ''
+            const wordList = allText.split(' ')
+            wordList.forEach(word => {
+                if (word.toLocaleLowerCase() === loopList[0].text.toLocaleLowerCase()) {
+                    htmlText += ` <b>${word}</b>`
+                }
+                else {
+                    htmlText += ' ' + word
+                }
+            });
 
+
+            $("#all-text").html(htmlText);
+            loopList = loopList.splice(1, loopList.length);
+        }
+    };
+    video.onended = function () {
+        if (loopList && loopList.length > 0) {
+            let source = $('<source>')
+            source.attr('src', 'assets/signs/' + loopList[0].file)
+            $(video).html(source);
+            video.load();
+            video.play();
+        }
+    };
+    // let audioChunks = [];
+
+    // audioRecorder.addEventListener("dataavailable", event => {
+    //     audioChunks.push(event.data);
+    //     console.log('test');
+    // });
+    // audioRecorder.addEventListener("stop", event => {
+    //     const audioBlob = new Blob(audioChunks);
+    //     const audioUrl = URL.createObjectURL(audioBlob);
+    //     const audio = new Audio(audioUrl);
+    //     audio.play();
+    //     audioChunks = [];
+    // });
+
+    //END SES KAYDETME
+
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+
+    let allText = '';
     let isRecord = false;
+    let loopList = [];
     microphone.click(() => {
         if (isRecord) {
-            isRecord = false;
-            microphone.removeClass('btn-danger');
-            microphone.addClass('btn-success');
-            microphoneText.text("")
-            audioRecorder.stop();
+            recognition.stop();
         }
         else {
-            isRecord = true;
-            
-            microphone.addClass('btn-danger');
-            microphone.removeClass('btn-success');
-            audioRecorder.start();
+            recognition.start();
         }
     })
 
-    let audioChunks = [];
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
 
-    audioRecorder.addEventListener("dataavailable", event => {
-        audioChunks.push(event.data);
-        console.log('test');
-    });
-    audioRecorder.addEventListener("stop", event => {
-        const audioBlob = new Blob(audioChunks);
-        const audioUrl = URL.createObjectURL(audioBlob);
-        const audio = new Audio(audioUrl);
-        audio.play();
-        audioChunks = [];
+        allText = transcript.toLocaleLowerCase();
+    })
+
+    recognition.addEventListener('start', () => {
+        isRecord = true;
+        microphone.addClass('btn-danger');
+        microphone.removeClass('btn-success');
+        microphoneText.text("")
     });
 
-    //END SES KAYDETME
+    recognition.addEventListener('end', () => {
+        isRecord = false;
+        microphone.removeClass('btn-danger');
+        microphone.addClass('btn-success');
+        microphoneText.text("")
+        $("#all-text").text(allText);
+
+        let words = allText.split(' ');
+
+        loopList = [];
+        for (const key in words) {
+
+            let word = list.find(m => m.text.toLocaleLowerCase() === words[key].toLocaleLowerCase())
+            if (word) {
+                loopList.push(word)
+            }
+
+        }
+
+        let source = $('<source>')
+        source.attr('src', 'assets/signs/' + loopList[0].file)
+        $(video).html(source);
+
+        video.load();
+        video.play();
+    });
 
 })
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -17,40 +17,7 @@ function firstInit(){
 }
 
 
-function speechToText() {
 
-    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-    const recognition = new SpeechRecognition();
-    recognition.interimResults = true;
-
-    let p = document.createElement("span")
-    const words = document.querySelector(".words")
-    words.appendChild(p)
-
-    recognition.addEventListener('result', e => {
-        const transcript = Array.from(e.results)
-            .map(result => result[0])
-            .map(result => result.transcript)
-            .join('')
-
-        p.textContent = transcript;
-        //console.log(transcript);
-        if (e.results[0].isFinal) {
-            words.innerHTML = "";
-            p = document.createElement('span');
-            words.appendChild(p)
-        }
-    })
-
-    recognition.addEventListener('end', () => {
-        recognition.start();
-        //console.log("recognition tekrar başlatıldı.");
-    });
-
-    recognition.start()
-
-}
 
 firstInit()
 
